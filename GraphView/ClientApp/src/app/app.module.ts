e6822b5f49759da1f9c0c6e30343923c3231b2b7
4reeetr0 <88400860+4reeetr0@users.noreply.github.com>
2023-05-18 14:49:57 +0300

    Removed unsused components

--- GraphView/ClientApp/src/app/app.module.ts
+++ GraphView/ClientApp/src/app/app.module.ts
@@ -7,25 +7,19 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { GraphsComponent } from './graphs/graphs.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     GraphsComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: GraphsComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: GraphsComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
